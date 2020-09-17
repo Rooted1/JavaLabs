6a7d166f68874ec708811b719010aaaf39514048
Ruth Obe <ruthobey@gmail.com>
2020-09-17 04:59:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    final int NUMBER_OF_DEPTS = 20;
+    final double PI = 3.14159;
+    final double TAX_RATE = 0.015;
+    final String COMPANY = "ABC Manufacturing";
+
+    System.out.println("Hello world! " + COMPANY);
+    System.out.println(COMPANY);
+  }
+}
