e7b0a701f7639316a59ab58966802742f45f606f
Ruth Obe <ruthobey@gmail.com>
2020-09-17 05:02:36 +0000

    add named constant to DataDemo

--- DataDemo.java
+++ DataDemo.java
@@ -1,8 +1,10 @@
 public class DataDemo {
   public static void main (String[] args) {
     int aWholeNumber = 315;
+    final int STATE_IN_US = 50;
 
     System.out.print("The number is ");
     System.out.print(aWholeNumber);
+    System.out.print("The number of states in the US is " + STATE_IN_US)
   }
 }
